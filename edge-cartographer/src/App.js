adf38ccffe508950687f2f52969145fb4a949de7
Matthew McClure <106849204+mwmcclure7@users.noreply.github.com>
2024-06-13 14:40:41 -0400

    fixed img width crashing issue

--- edge-cartographer/src/App.js
+++ edge-cartographer/src/App.js
@@ -8,18 +8,16 @@ function mapRange(value, fromSource, toSource, fromTarget, toTarget) {
 function App() {
   const [location, setLocation] = useState({ lat: null, lng: null });
   const [circlePosition, setCirclePosition] = useState({ top: "0px", left: "0px"});
-  var imgRef = useRef();
 
   useEffect(() => {
     if ("geolocation" in navigator) {
       navigator.geolocation.watchPosition(
         function(position) {
-          setLocation({ lat: position.coords.latitude, lng: position.coords.longitude });
-          if (imgRef.current) {
-            setCirclePosition({
-              top: `${mapRange(location.lat, 42.426613, 42.409726, 0, imgRef.current.offsetWidth)}px`,
-              left: `${mapRange(location.lng, -83.483743, -83.471405, 0, imgRef.current.offsetHeight)}px` });
-          }
+          const { latitude, longitude } = position.coords;
+          setLocation({ lat: latitude, lng: longitude });
+          setCirclePosition({
+            top: `${mapRange(latitude, 42.426613, 42.409726, 0, 0.53457943925*window.innerHeight)}px`,
+            left: `${mapRange(longitude, -83.483743, -83.471405, 0, window.innerHeight)}px` });
         },
         function(error) {
           console.error("Error getting user location:", error);
@@ -33,7 +31,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img ref={imgRef} src="map.jpg" className="App-map" alt="cass-benton-map" />
+        <img src="map.jpg" className="App-map" alt="cass-benton-map" />
         <div className="circle" style={circlePosition}></div>
       </header>
     </div>
